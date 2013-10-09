e04c1b04e80f04b88a2e9e637300229ffe86ec48
Pavel Tisunov <tisunov.pavel@gmail.com>
2013-10-09 15:17:10 +0400

    Initial Commit

---
+++ Hopper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hopper
+//
+//  Created by Pavel Tisunov on 10/9/13.
+//  Copyright (c) 2013 Bright Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HopperTests/HopperTests.m
@@ -0,0 +1,34 @@
+//
+//  HopperTests.m
+//  HopperTests
+//
+//  Created by Pavel Tisunov on 10/9/13.
+//  Copyright (c) 2013 Bright Stripe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HopperTests : XCTestCase
+
+@end
+
+@implementation HopperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
